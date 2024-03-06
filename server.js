693fed02304bc282662eff94e72b0df42fe6b054
ayush2481 <ayushsingla5552@gmail.com>
2024-03-06 20:00:28 +0530

    edit changes

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.get("/", function (req, resp) {
 });
 
 app.get("/saveProfile", function (req, resp) {
-    const email = req.query.txtEmail;
-    const password = req.query.txtPwd;
+    const emailiiiid = req.query.txtEmail;
+    const passwords = req.query.txtPwd;
     const usertype = req.query.utype;
     const status = 1;
 
-    mysql.query("Insert into users values(?,?,?,current_date(),?)", [email, password, usertype, status], function (err) {
+    mysql.query("Insert into users values(?,?,?,current_date(),?)", [emailiiiid, passwords, usertype, status], function (err) {
         if (err == null) {
             resp.send("Sign Up Successfully!!!");
         }
@@ -50,7 +50,7 @@ app.get("/saveProfile", function (req, resp) {
 })
 //------------------------------------------------
 app.get("/check-User", function (req, resp) {
-    mysql.query("select * from users where emailid=?", [req.query.kuchEmail], function (err, resultJsonArray) {
+    mysql.query("select * from users where emailid=?", [req.query.KUCHEmail], function (err, resultJsonArray) {
         if (resultJsonArray.length == 1)
             resp.send("Email already exists");
         else
@@ -85,9 +85,9 @@ app.get("/change-pass", function (req, resp) {
     })
 })
 //------------------------------------------------
-app.get("/profileLogin", function (req, resp) {
-    const emaill = req.query.Emailiid;
-    const passwor = req.query.Passwords;
+app.post("/profileLogin", function (req, resp) {
+    const emaill = req.body.Email;
+    const passwor = req.body.Password;
     mysql.query("select * from users where emailid=? and pwd=?", [emaill, passwor], function (err, resultJsonArray) {
         if (err) {
             
